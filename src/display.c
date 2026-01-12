4297acf07734bcf31db3562b914ee0108f5a3ac4
Lucas Chéron <lucas.cheron@etud.unistra.fr>
2026-01-12 16:39:02 +0100

    Ajout instructions affichage

--- src/display.c
+++ src/display.c
@@ -5,22 +5,25 @@
 
 int fd;
 
-void clean_display_ending(int sig) {
+void clean_display_ending(int sig)
+{
     close(fd);
     printf("Terminaison propre display\n");
     exit(EXIT_SUCCESS);
 }
 
-void display_loop(int fd_lecture) {
+void display_loop(int fd_lecture)
+{
     fd = fd_lecture;
 
     struct sigaction sa;
     sa.sa_handler = clean_display_ending;
     sigaction(SIGTERM, &sa, NULL);
 
-    while(1) {
+    while (1)
+    {
         game_infos_t game_info;
-        read(fd,&game_info,sizeof(game_infos_t));
+        read(fd, &game_info, sizeof(game_infos_t));
         display_game(&game_info);
     }
     clean_display_ending(0);
@@ -28,20 +31,25 @@ void display_loop(int fd_lecture) {
 
 void display_game(const game_infos_t *infos)
 {
-    //ctrl L pour afficher seuleument une grille
+    // ctrl L pour afficher seuleument une grille
     printf("\033[2J\033[H");
 
-    //affichage du jeu
+    // affichage du jeu
     printf("====== 2048 ======\n\n");
     printf("Score : %u\n\n", infos->score);
 
-    //affichage de la grille 
-    for (int i = 0; i < GRID_SIZE; i++) {
+    // affichage de la grille
+    for (int i = 0; i < GRID_SIZE; i++)
+    {
         printf("+------+------+------+------+ \n");
-        for (int j = 0; j < GRID_SIZE; j++) {
-            if (infos->grid[i][j] == 0) {
+        for (int j = 0; j < GRID_SIZE; j++)
+        {
+            if (infos->grid[i][j] == 0)
+            {
                 printf("| %4s ", " ");
-            } else {
+            }
+            else
+            {
                 printf("| %4u ", infos->grid[i][j]);
             }
         }
@@ -49,18 +57,20 @@ void display_game(const game_infos_t *infos)
     }
     printf("+------+------+------+------+\n\n");
 
-    //état du jeu
-    switch (infos->game_state) {
-        case STATE_NOT_FINISHED:
-            printf("Partie en cours...\n");
-            break;
-        case STATE_WIN:
-            printf("🎉 Victoire! Vous avez atteint 2048 !\n");
-            break;
-        case STATE_LOSE:
-            printf("💀 Défaite! Aucun coup possible.\n");
-            break;
+    // état du jeu
+    switch (infos->game_state)
+    {
+    case STATE_NOT_FINISHED:
+        printf("Partie en cours...\n");
+        break;
+    case STATE_WIN:
+        printf("🎉 Victoire! Vous avez atteint 2048 !\n");
+        break;
+    case STATE_LOSE:
+        printf("💀 Défaite! Aucun coup possible.\n");
+        break;
     }
+    printf("Appuyer sur Q pour quitter.\n");
 
     fflush(stdout);
 }
